2addced8e118b791dd1c9c02fb80ed7f4a8836ee
Santiago Diaz Muñoz <149324836+Diaz1010@users.noreply.github.com>
2024-02-23 21:17:20 +0000

    One

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const [task, setTask] = useState('');
+  	const [tasksList, setTasksList] = useState([]);
+
+	const handleTask = (e) => {
+		if (e.key == "Enter") {
+			setTasksList([...tasksList, 
+			{
+				label: task,
+				done: false,
+	
+			}])
+		}
+
+		setTask(e.target.value);
+	}
+
+	return (<>
+		<h1>To do List {task}</h1>
+		<input type="text" placeholder="Task" 
+		onKeyUp={ (evt)=> handleTask(evt) }/>
+
+		{tasksList.map((toDo , index)=> {
+			return <div key={index}>
+				{toDo.label}
+			</div>
+		})}
+	
+	</>
+
+	)
 };
 
 export default Home;
